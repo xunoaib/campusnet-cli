0b7460427f1b6d99f0c51f64747f11bbadb8f887
xunoaib <xunoaib@gmail.com>
2025-04-06 15:18:06 -0400

    make headers class variable

--- scrape.py
+++ scrape.py
@@ -16,6 +16,20 @@ import requests
 
 class CampusNet:
 
+    headers = {
+        "Accept": "*/*",
+        "Priority": "u=0",
+        "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0",
+        "Connection": "keep-alive",
+        "Referer":
+        "https://campusnet.csuohio.edu/sec/classsearch/search_reg.jsp",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Site": "same-origin",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Sec-Fetch-Mode": "cors"
+    }
+
     def __init__(self):
         self.session = requests.Session()
 
@@ -28,27 +42,10 @@ class CampusNet:
             "backdoor": "N",
             "browser": "Firefox"
         }
-        headers = {
-            "Origin": "https://campusnet.csuohio.edu",
-            "Accept":
-            "text/html,application/xhtml xml,application/xml;q=0.9,*/*;q=0.8",
-            "Priority": "u=0, i",
-            "User-Agent":
-            "Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0",
-            "Connection": "keep-alive",
-            "Referer": "https://campusnet.csuohio.edu/login.jsp",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-Mode": "navigate",
-            "Upgrade-Insecure-Requests": "1",
-            "Sec-Fetch-User": "?1",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
         response = self.session.post(
             "https://campusnet.csuohio.edu/ps8verify.jsp",
             data=paramsPost,
-            headers=headers)
+            headers=self.headers)
 
         if 'Login in progress' not in response.text:
             raise Exception('Login failed!')
